9e810ef92fcf0c3c3d0b1cefc738b2b823f3e5e2
Aracely Alba <aracelyalba8@gmail.com>
2023-05-03 13:57:03 -0400

    Added cors

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const { configOptions } = require('./config/config.js');
 const routerApi = require('./routes/index.js');
 const { legErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler.js');
@@ -6,7 +7,20 @@ const { legErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('
 const app = express();
 const port = configOptions.port;
 
+const whiteList = ['http://localhost:3000'];
+
+const corsOptions = {
+
+  origin: (origin, callback) => {
+
+    (whiteList.indexOf(origin) !== -1 || !origin)
+      ? callback(null, true)
+      : callback(new Error('Not allowed by CORS'));
+  }
+};
+
 app.use(express.json());
+app.use(cors());
 
 app.get('/api', (req, res) => res.send('HOME'));
 routerApi(app);
